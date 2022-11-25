f611fd97518adc0281a3ef0230f3edb4058bb195
lokkib <lokkichebrikov@yandex.ru>
2022-11-25 15:41:00 +0300

    creating-server-node.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-const http = require('http');
+const http = require("http");
+const getUsers = require("./modules/users");
 
 const server = http.createServer((request, response) => {
+  if (request.url != "/favicon.ico") {
+    const url = new URL(request.url, "http://127.0.0.1:3003");
+    const helloRequest = url.searchParams.has("hello");
+    const name = url.searchParams.get("hello");
+    const users = url.searchParams.has("users");
 
-    // Написать обработчик запроса:
-    // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
-    // - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
-    // - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
-    // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
-    // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
+    if (helloRequest) {
+      if (name) {
+        response.writeHead(200, { "Content-Type": "text/plain" });
+        response.write(`Hello,${name}`);
+        response.end();
+      } else if (!name) {
+        response.writeHead(400, { "Content-Type": "text/plain" });
+        response.write("Enter a name");
+        response.end();
+      }
+    } else if (users) {
+      response.writeHead(200, { "Content-Type": "application/json" });
+      response.write(getUsers());
+      response.end();
+    } else if (request.url === "/") {
+      response.writeHead(200, { "Content-Type": "text/plain" });
+      response.write("Hello, World!");
+      response.end();
+    } else if (url.searchParams !== "hello" && url.searchParams !== "name") {
+      response.writeHead(500, { "Content-Type": "text/plain" });
+      response.write("");
+      response.end();
+    }
+  }
+});
 
+server.listen(3003, () => {
+  console.log("Сервер запущен по адресу http://127.0.0.1:3003");
 });
